e227166dd9ace074747a32290d096e9e363a6ae9
G01111100 <145698991+G01111100@users.noreply.github.com>
2023-09-25 13:25:36 +0700

    v2.2 add main in commit

--- main.py
+++ main.py
@@ -16,29 +16,29 @@ dp = Dispatcher()
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("*It's just begin!*")
+    await message.answer("*It's just begin*")
 
 # Хэндлер на команду /Answer
 @dp.message(Command("answer"))
 async def cmd_answer(message: types.Message):
-    await message.answer("Your answer!")
+    await message.answer("Your answer\!")
 
 # Хэндлер на команду /Reply
 async def cmd_reply(message: types.Message):
-    await message.reply("Your reply!")
+    await message.reply("Your reply\!")
 
 @dp.message(Command("testText"))
 async def cmd_testText(message: types.Message):
-    # await message.answer("<i>Hello<i>, <b>world</b>!")
+    await message.answer("<i>Hello</i>, <b>world</b>!", parse_mode="html")
     await message.answer("_Hello_, *world*\! __I am__  ~here~")
 
 @dp.message(Command("name"))
 async def cmd_name(message: types.Message, command: CommandObject):
     if command.args:
-        #await message.answer(f"Привет, _*{command.args}*_")
+        # await message.answer(f"Привет, _*{command.args}*_")
         await message.answer(f"Привет, {html.bold(html.quote(command.args))}", parse_mode="html")
     else:
-        await message.answer("Пожалуйста, укажи своё имя после команды /name!")
+        await message.answer("Пожалуйста, укажи своё имя после команды /name\!")
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
