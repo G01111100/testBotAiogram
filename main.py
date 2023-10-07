9a2c241ae292c2c5524aa1aa567c8578117a8883
G01111100 <145698991+G01111100@users.noreply.github.com>
2023-10-07 22:03:04 +0700

    v.7 Special button

--- main.py
+++ main.py
@@ -91,11 +91,64 @@ async def reply_builder(message: types.Message):
     builder = ReplyKeyboardBuilder()
     for i in range(1, 17):
         builder.add(types.KeyboardButton(text=str(i)))
-    builder.add(types.KeyboardButton(text=str("Cansel")))
+    builder.add(types.KeyboardButton(text=str("Cancel")))
     builder.adjust(8, 8, 1)
     await message.answer(
         "Выберите число:",
-        reply_markup=builder.as_markup(resize_keyboard=True) #, one_time_keyboard=True),#selective - for pool of users
+        reply_markup=builder.as_markup(resize_keyboard=True)#, one_time_keyboard=True),#selective - for pool of users
+    )
+
+@dp.message(Command("specialbuttons"))
+async def cmd_special_buttons(message: types.Message):
+    builder = ReplyKeyboardBuilder()
+    # метод row позволяет явным образом сформировать ряд
+    # из одной или нескольких кнопок. Например, первый ряд
+    # будет состоять из двух кнопок...
+    builder.row(
+        types.KeyboardButton(text="Запросить геолокацию", request_location=True),
+        types.KeyboardButton(text="Запросить контакт", request_contact=True)
+    )
+    # ... второй из одной ...
+    builder.row(types.KeyboardButton(
+        text="Создать викторину",
+        request_poll=types.KeyboardButtonPollType(type="quiz"))
+    )
+    # ... а третий снова из двух
+    builder.row(
+        types.KeyboardButton(
+            text="Выбрать премиум пользователя",
+            request_user=types.KeyboardButtonRequestUser(
+                request_id=1,
+                user_is_premium=True
+            )
+        ),
+        types.KeyboardButton(
+            text="Выбрать супергруппу с форумами",
+            request_chat=types.KeyboardButtonRequestChat(
+                request_id=2,
+                chat_is_channel=False,
+                chat_is_forum=True
+            )
+        )
+    )
+
+    await message.answer(
+        "Выберите действие:",
+        reply_markup=builder.as_markup(resize_keyboard=True),
+    )
+
+@dp.message(F.user_shared)
+async def on_user_shared(message: types.Message):
+    print(
+        f"Request {message.user_shared.request_id}. "
+        f"User ID: {message.user_shared.user_id}"
+    )
+
+@dp.message(F.chat_shared)
+async def on_user_shared(message: types.Message):
+    print(
+        f"Request {message.chat_shared.request_id}. "
+        f"User ID: {message.chat_shared.chat_id}"
     )
 
 @dp.message(F.animation)
@@ -128,9 +181,9 @@ async def with_puree(message: types.Message):
 async def without_puree(message: types.Message):
     await message.reply("Так невкусно\!")
 
-@dp.message(F.text.lower() == "cansel")
+@dp.message(F.text.lower() == "cancel")
 async def without_puree(message: types.Message):
-    await message.reply("Cansel\!", reply_markup=types.ReplyKeyboardRemove())
+    await message.reply("Cancel\!", reply_markup=types.ReplyKeyboardRemove())
 
 async def without_puree(message: types.Message):
     await message.reply("Так невкусно\!")
@@ -151,8 +204,6 @@ async def somebody_added(message: types.Message):
         # проперти full_name берёт сразу имя И фамилию
         # (на скриншоте выше у юзеров нет фамилии)
         await message.reply(f"Hello there\!, {user.full_name} ")
-
-
 
 #Don't check-------------------------------------------------------------------
 
